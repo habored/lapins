fbf6d6374823c71528103f0dfae9ee0c3f617bd8
bouillotvincent <vincent.bouillot.sa@gmail.com>
2021-07-05 09:55:12 +0200

    test sandbox

--- docs/xtra/javascripts/interpreter.js
+++ docs/xtra/javascripts/interpreter.js
@@ -38,7 +38,16 @@ pyodide.runPython(`
 `, namespace);
 namespace.destroy();
 
+await pyodideReadyPromise;
 
+// const url1 = 'https://raw.githubusercontent.com/bouillotvincent/bouillotvincent.github.io/master/js-turtle.py'
+const url1 = 'https://glcdn.githack.com/bouillotvincent/pyodide-mkdocs/-/raw/main/js-turtle.py'
+const response = await fetch(url1);
+const data = await response.text();
+console.log(data);
+
+pyodide.runPython(data);
+ 
 let my_module = {
 
     tortue : class {
